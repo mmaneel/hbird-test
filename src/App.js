134878c487400628d10413a0a8cce1e33677d902
manel <m.mohandouali@esi-sba.dz>
2022-04-05 14:35:30 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ReactDOM } from 'react';
+
+class App extends React.Component() {
+  constructor(props){
+
+     super(props)
+  
+    this.state = {
+       isLogginActive:true,
+    }
+  }
+  render(){
+    const{isLogginActive}=this.state;
+    return (
+      <div className='App'>
+      <LoginForm/>
+      {isLogginActive &&  <LoginForm/>}
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,66 @@
+import React from "react";
+import react ,{useState} from 'react';
+import LOGO from "../LOGO.png";
+import usern from "../usern.png";
+import passw from "../passw.png";
+import akar from "../akar.png";
+import fcb from "../fcb.png"
+import git from "../git.png"
+import google from "../google.png"
+
+function LoginForm(){
+    return(
+        <div>
+        <form id="login">
+             <div className="loginFields">
+                <div className="button-box " ref={this.props.containerRef}>
+                    <div id="bttn"></div>  
+                    <button className="btn">Login</button>
+                    <button className="btn" >Register</button>
+                </div>
+                <button className="back"><img src={akar}  />  </button> 
+            </div>
+
+            <div className="logo">
+                <img  src={LOGO} />
+                <h1>Welcome</h1>
+                <p>Log in to your account to continue </p>
+            </div>
+            
+            <div className="formGroup">
+                <div className="formItem">
+                    <img className="icon1" src={usern} />
+                    <input type={"text"} className="name" placeholder="User Name"/>
+
+                </div>
+                <div className="formItem">
+                     <img  className="icon2" src={passw} />
+                     <input type={"password"} className="name" placeholder="Password"/>
+
+                </div>
+                
+                <a href="#" className="forgotPass">Forgot your password?</a>
+            </div>
+            <div className="footer">
+                <button className="loginBtn" >Log in</button>
+            </div>
+            <div className="connect">
+                <p>
+                    Or connect with
+                </p>
+                <div className="sclm">
+                    <a href="https://fr-fr.facebook.com/" className="bac"><img src={fcb}  />  </a>
+                    <a  href="https://github.com/" className="bac"><img src={git}  />  </a>
+                    <a  href ="#" className="bac"><img src={google}  />  </a>
+                </div>
+
+            </div>
+            
+        </form>
+
+        
+   </div>
+        
+    )
+}
+export default LoginForm
